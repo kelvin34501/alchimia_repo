4f02d76f3b151e55e0948a3ae6196113420fef72
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2019-05-13 22:15:53 +0800

    debug for model control and more comments

--- pythonsupport/windowsPython.cpp
+++ pythonsupport/windowsPython.cpp
@@ -1,31 +1,41 @@
 #include "pythonadapter.h"
 #include <iostream>
-#include<Windows.h>
+#include <Windows.h>
+#include <cstring>
 
 using namespace std;
 
 int WindowsPython::runPython(const char* file_path){
-    LPTSTR sConLin = (pypath + " \"" + file_path + "\"").c_str;
+    LPTSTR sConLin = (LPTSTR)((pypath + " \"" + file_path + "\"").c_str());
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
- 
+    bool flag;
+
 	ZeroMemory(&si, sizeof(si));
 	ZeroMemory(&pi, sizeof(pi));
+
+    Sleep(500);
 
     if(CreateProcess(NULL, sConLin, NULL, NULL, false, 0, NULL, NULL, &si, &pi))
 	{
 		cout << "create process success" << endl;
         WaitForSingleObject(pi.hProcess, INFINITE);
+        flag = 1;
 	}
 	else{
 		cerr << "failed to create process" << endl;
+        flag = 0;
 	}
 
+    Sleep(500);
+
 	TerminateProcess(pi.hProcess, 300);
+
+    return flag;
 }
 
 int WindowsPython::activateTB(const char* log_dir){
-
+    return 0;
 }
 
 void WindowsPython::setPythonPath(const char* path){
