b0fb2286217b2045391d45209f82475113d6d266
Shining Xu <xushangning@sjtu.edu.cn>
2019-05-14 18:40:14 +0800

    Assign ID to PartItem

--- app/editorcontrol.cpp
+++ app/editorcontrol.cpp
@@ -6,8 +6,8 @@
 
 void EditorControl::add(PartType partType, const QPointF &pos) const
 {
-    /* int id = */mGraphModel.addPart(partType, static_cast<float>(pos.x()), static_cast<float>(pos.y()));
-    mModelScene.addItem(new PartItem(0, pos));
+    int id = mGraphModel.addPart(partType, static_cast<float>(pos.x()), static_cast<float>(pos.y()));
+    mModelScene.addItem(new PartItem(id, pos));
     mToolboxButtonGroup.checkedButton()->setChecked(false);    // uncheck the button
     mModelScene.setClickMode(ModelScene::Idle);
 }
