56bd026c4eb1930d55f6b7fa8df73080a9c600c7
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2019-05-18 14:03:55 +0800

    update windowsPython for TB, include model control in app

--- app/pythonsupport/windowsPython.cpp
+++ app/pythonsupport/windowsPython.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int WindowsPython::runPython(const char* file_path){
-    LPTSTR sConLin = (LPTSTR)((pypath + " \"" + file_path + "\"").c_str());
+    LPTSTR sConLin = (LPTSTR)(("\"" + pypath + "\" \"" + file_path + "\"").c_str());
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
     bool flag;
@@ -35,9 +35,38 @@ int WindowsPython::runPython(const char* file_path){
 }
 
 int WindowsPython::activateTB(const char* log_dir){
-    return 0;
+    LPTSTR sConLin = (LPTSTR)(("\"" + tbpath + "\" \"" + log_dir + "\"").c_str());
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+    bool flag;
+
+	ZeroMemory(&si, sizeof(si));
+	ZeroMemory(&pi, sizeof(pi));
+
+    Sleep(500);
+
+    if(CreateProcess(NULL, sConLin, NULL, NULL, false, 0, NULL, NULL, &si, &pi))
+	{
+		cout << "create process success" << endl;
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        flag = 1;
+	}
+	else{
+		cerr << "failed to create process" << endl;
+        flag = 0;
+	}
+
+    Sleep(500);
+
+	TerminateProcess(pi.hProcess, 300);
+
+    return flag;
 }
 
 void WindowsPython::setPythonPath(const char* path){
     pypath = path;
+}
+
+void WindowsPython::setTBPath(const char* path){
+    tbpath = path;
 }
