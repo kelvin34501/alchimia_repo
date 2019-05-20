32f00d4ab5c579e35d3ad3561cc3298d48571459
HesperX <48796750+HesperX@users.noreply.github.com>
2019-05-20 21:01:29 +0800

    Update tst_case.h

--- test/tst_case.h
+++ test/tst_case.h
@@ -183,9 +183,108 @@ TEST(addConnection,set)
 //    compare_map(oracle,gm.getPartInfo(5));
 //    oracle.clear();
 
+//    gm.addConnection(0,6,1,0);
+//    oracle["num_input_port"] = "1";
+//    oracle["num_output_port"] = "1";
+//    oracle["shape_input_port_0"] = "None,256,256,3,";
+//    oracle["shape_output_port_0"] = "None,64,";
 
-
+//    compare_map(oracle,gm.getPartInfo(6));
+//    oracle.clear();
 }
+
+TEST(editPart,set)
+{
+    GraphModel gm(Backend::Keras);
+    Part *pa;
+    Connection *cn;
+    map<string,string> oracle;
+
+    gm.addPart(PartType::InputLayer);
+    gm.addPart(PartType::Conv2D);
+    gm.addPart(PartType::Activation);
+    gm.addPart(PartType::Dropout);
+    gm.addPart(PartType::Reshape);
+    gm.addPart(PartType::Dense);
+
+//    gm.editPart(0,"input_shape","(128,128,3)");
+//    oracle["num_input_port"] = "0";
+//    oracle["num_output_port"] = "1";
+//    oracle["shape_output_port_0"] = "None,128,128,3,";
+//    compare_map(oracle,gm.getPartInfo(0));
+//    oracle.clear();
+
+    gm.editPart(1,"filters","64");
+    gm.editPart(1,"kernel_size","(2,2)");
+    gm.editPart(1,"strides","(2,2)");
+
+
+    oracle["num_input_port"] = "1";
+    oracle["num_output_port"] = "1";
+    oracle["shape_input_port_0"] = "";
+    oracle["shape_output_port_0"] = "";
+    oracle["filters"] = "64";
+    oracle["kernel_size"] = "(2,2)";
+    oracle["strides"] = "(2,2)";
+    compare_map(oracle,gm.getPartInfo(1));
+    oracle.clear();
+
+    gm.editPart(3,"rate","0.3");
+    oracle["rate"] = "0.3";
+    compare_map(oracle,gm.getPartInfo(3));
+    oracle.clear();
+
+    gm.editPart(4,"target_shape","(2,-1)");
+    oracle["target_shape"] = "(2,-1)";
+    compare_map(oracle,gm.getPartInfo(4));
+    oracle.clear();
+
+    gm.addConnection(0,1,1,0);
+    oracle["num_input_port"] = "0";
+    oracle["num_output_port"] = "1";
+    oracle["shape_input_port_0"] = "";
+    oracle["shape_output_port_0"] = "None,256,256,3,";
+
+    compare_map(oracle,gm.getPartInfo(0));
+    oracle.clear();
+
+    gm.addConnection(1,2,1,0);
+    oracle["num_input_port"] = "1";
+    oracle["num_output_port"] = "1";
+    oracle["shape_input_port_0"] = "None,256,256,3,";
+    oracle["shape_output_port_0"] = "None,255,255,64,";
+
+    compare_map(oracle,gm.getPartInfo(1));
+    oracle.clear();
+
+    gm.addConnection(2,3,1,0);
+    oracle["num_input_port"] = "1";
+    oracle["num_output_port"] = "1";
+    oracle["shape_input_port_0"] = "None,255,255,64,";
+    oracle["shape_output_port_0"] = "None,255,255,64,";
+
+    compare_map(oracle,gm.getPartInfo(2));
+    oracle.clear();
+
+    gm.addConnection(4,3,0,1);
+    oracle["num_input_port"] = "1";
+    oracle["num_output_port"] = "1";
+    oracle["shape_input_port_0"] = "None,255,255,64,";
+    oracle["shape_output_port_0"] = "None,255,255,64,";
+
+    compare_map(oracle,gm.getPartInfo(3));
+    oracle.clear();
+
+    gm.addConnection(4,5,1,0);
+    oracle["num_input_port"] = "1";
+    oracle["num_output_port"] = "1";
+    oracle["shape_input_port_0"] = "None,255,255,64,";
+    oracle["shape_output_port_0"] = "None,2,2080800,";
+
+    compare_map(oracle,gm.getPartInfo(4));
+    oracle.clear();
+}
+
 
 void compare_map(map<string,string> m1, map<string,string> m2)
 {
