79942045bd8717b22b144132f1e09f7a5a2ffe8c
Shining Xu <xushangning@sjtu.edu.cn>
2019-05-20 22:26:47 +0800

    Check for the case when there is no part under the cursor during connection

--- app/modelscene.cpp
+++ app/modelscene.cpp
@@ -35,15 +35,19 @@ void ModelScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
         // PartItems at the start and end of incompleteConnection
         // The topmost item is incompleteConnection
         QList<QGraphicsItem *> startItems = items(incompleteConnection->line().p1());
-        // TODO: Check empty lists
         startItems.pop_front();
-        PartItem *start = static_cast<PartItem *>(startItems.front());
         QList<QGraphicsItem *> endItems = items(incompleteConnection->line().p2());
         endItems.pop_front();
-        PartItem *end = static_cast<PartItem *>(endItems.front());
-
         removeItem(incompleteConnection);
         delete incompleteConnection;
+
+        // Don't change the state of ModelScene if there is no part under
+        // the cursor.
+        if (startItems.empty() || endItems.empty())
+            return;
+
+        PartItem *start = static_cast<PartItem *>(startItems.front());
+        PartItem *end = static_cast<PartItem *>(endItems.front());
 
         int id = mProject.graph_mdl->addConnection(start->id(), end->id(), true, false);
         addItem(new ConnectionItem(*start, *end, id));
